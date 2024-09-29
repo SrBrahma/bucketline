108e219faad3aa6d1ce0810877cc8891df8d122e
SrBrahma <henrique.bruno.fa@gmail.com>
2024-09-29 10:51:43 +0200

    git minor changes

--- src/tests/vite.test.ts
+++ src/tests/vite.test.ts
@@ -1,15 +1,20 @@
 import { test } from "bun:test"
 import path from "path"
+import fse from "fs-extra"
 import simpleGit from "simple-git"
 import { testsPath } from "../utils/consts"
 
 const vitePath = path.resolve(testsPath, "vite")
+const viteE2ePath = path.resolve(vitePath, "e2e")
 const shell = Bun.$.cwd(vitePath)
 const git = simpleGit().cwd(vitePath)
 
 const setup = async () => {
   await git.init()
+  if (!(await git.status()).isClean())
+    throw new Error("vite test git is not clean!")
   await git.commit("initial commit", [], { "--allow-empty": null })
+  await fse.remove(viteE2ePath)
 }
 
 test("vite works", async () => {
--- tests/vite/e2e/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test"
-
-test("has title", async ({ page }) => {
-  await page.goto("/")
-  await expect(page.getByText("Vite + React")).toBeVisible()
-  await expect(page).toHaveScreenshot("00-my-first-screenshot.png")
-})
